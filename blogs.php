139df6ac347ab7497079f7b5bcb5622a14c5b78f
RASHED <jmrashed@gmail.com>
2023-10-10 03:05:42 +0600

    update

---
+++ blogs.php
@@ -0,0 +1,24 @@
+<?php include('header.php'); ?>
+<!-- Hero Area Start-->
+<div class="hero-area2 d-flex align-items-center ">
+    <div class="container">
+        <div class="row ">
+            <div class="col-xl-12">
+                <!-- Hero Caption -->
+                <div class="hero-cap pt-50">
+                    <h2>Blog</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Blog Area -->
+<div class="blog ">
+    <div class="container">
+        <div class="row" id="blog-list">
+        </div>
+    </div>
+</div>
+
+<!--Hero Area End-->
+<?php include('footer.php');
